c6c809597f68bb2a30a54edfc79772116519d401
MACXDON <72674976+MACXDON@users.noreply.github.com>
2022-03-31 04:04:23 -0500

    Added Logic

---
+++ src/game/vector.js
@@ -0,0 +1,24 @@
+// Vector class is used for directions and coordinates
+
+export default class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    scaleBy(number) {
+        return new Vector(this.x * number, this.y * number);
+    }
+
+    length() {
+        return Math.hypot(this.x, this.y);
+    }
+
+    add({ x, y }) {
+        return new Vector(this.x + x, this.y + y);
+    }
+
+    normalize() {
+        return this.scaleBy(1 / this.length());
+    }
+}
